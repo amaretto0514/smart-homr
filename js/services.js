84fdf8ccd494de879a83b29baae161a9abf2e73e
Víctor Agustín Villalta Delgado <116325350+amaretto0514@users.noreply.github.com>
2023-06-05 20:01:01 -0500

    first commit

---
+++ js/services.js
@@ -0,0 +1,25 @@
+'use strict'
+//Barra de navegacion
+window.addEventListener("scroll",function(){
+    var header=this.document.querySelector("header");
+    header.classList.toggle('down',window.scrollY>0)
+
+    var logo = this.document.querySelector(".brand img");
+    if(window.scrollY>0){
+        logo.setAttribute('src','/img/logo-1.png')
+    }else{
+        logo.setAttribute('src','/img/logo-alternativo-1.png')
+    }
+});
+
+var menu = this.document.querySelector('.menu')
+var menuBtn = this.document.querySelector('.menu-btn')
+var closeBtn = this.document.querySelector('.close-btn')
+
+menuBtn.addEventListener("click",() =>{
+    menu.classList.add('activate');
+})
+
+closeBtn.addEventListener("click",() =>{
+    menu.classList.remove('activate');
+})
